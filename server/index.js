18851afce0a0d2d69aaa71c7b9a71e7e98a241e2
Álvaro Nieto <agnieto00@gmail.com>
2021-10-29 17:54:31 +0200

    Added basic config for backend and frontend

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+app.get("/api", (req, res) => {
+    res.json( {message: "Hello form server"});
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+})
