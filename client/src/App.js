b7bd37ef73874d8a6ae62e9bc1ddcd339d91f190
Álvaro Nieto <agnieto00@gmail.com>
2021-10-30 15:35:46 +0200

    Added form

--- client/src/App.js
+++ client/src/App.js
@@ -32,7 +32,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? 'Loading...' : data}</p>
+        {/* <p>{!data ? 'Loading...' : data}</p> */}
         <LoginScreen authenticate={authenticate}/>
       </header>
     </div>
