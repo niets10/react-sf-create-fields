23ad3fecd29789e5eb3b5c2d606afeae6ba60e55
Álvaro Nieto <agnieto00@gmail.com>
2022-01-18 21:32:58 +0100

    Remove comments

--- client/src/components/loginScreen.jsx
+++ client/src/components/loginScreen.jsx
@@ -51,36 +51,5 @@ class LoginScreen extends Component {
         );
     }
 }
-
-// Function vs Class
-// function LoginScreen(props) {
-//     return (
-//         <div>
-//             <form>
-//                 <div class="mb-3">
-//                     <input type="email" placeholder="Username" class="form-control" id="sf-username" aria-describedby="emailHelp"/>
-//                 </div>
-//                 <div class="mb-3">
-//                     <input type="password" placeholder="Password" class="form-control" id="sf-password"/>
-//                 </div>
-//                     <div class="mb-3">
-//                         <input type="token" placeholder="Token" class="form-control" id="sf-token"/>
-//                     </div>
-//                     <div class="mb-3">
-//                     <div class="form-check form-check-inline">
-//                         <input class="form-check-input" type="radio" name="inlineRadioOptions" id="sf-production-check" value="sf-production"/>
-//                         <label class="form-check-label" for="Production">Production</label>
-//                     </div>
-//                     <div class="form-check form-check-inline">
-//                         <input class="form-check-input" type="radio" name="inlineRadioOptions" id="sf-production-check" value="sf-sandbox"/>
-//                         <label class="form-check-label" for="Sandbox">Sandbox</label>
-//                     </div>
-//                 </div>
-//                 <button onClick={getInfo}>Get info</button>
-//                 <button class="btn btn-primary" onClick={props.authenticate}>Authenticate</button>
-//             </form>
-//         </div>
-//     );
-// }
  
 export default LoginScreen;
